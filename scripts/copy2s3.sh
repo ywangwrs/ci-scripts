e4b8354cfb2858679bb597e3d5854fca9ed6d707
Ubuntu <ubuntu@ip-172-31-41-29.us-west-2.compute.internal>
2019-02-25 20:00:07 +0000

    test5

--- scripts/copy2s3.sh
+++ scripts/copy2s3.sh
@@ -60,10 +60,10 @@ copy2s3() {
 	       -o -name local.conf"
     echo "LOCAL_RSYNC_DIR=$LOCAL_RSYNC_DIR"
     find "$LOCAL_RSYNC_DIR" -name *Image
-    echo "docker run governmentpaas/awscli aws s3 cp /opt/rsync/builds/genericx86-64_wrlinux_image-glibc-std/genericx86-64_wrlinux_image-glibc-std/bzImage s3://${S3_BUCKET}/${S3_BUCKET_DIR}/"
-    docker run governmentpaas/awscli aws s3 cp /opt/rsync/builds/genericx86-64_wrlinux_image-glibc-std/genericx86-64_wrlinux_image-glibc-std/bzImage s3://${S3_BUCKET}/${S3_BUCKET_DIR}/
-    find "$LOCAL_RSYNC_DIR" \( $search_files \) -exec docker run governmentpaas/awscli aws s3 cp {} s3://${S3_BUCKET}/${S3_BUCKET_DIR}/ \;
-    docker run governmentpaas/awscli aws s3 ls --recursive s3://${S3_BUCKET}/${S3_BUCKET_DIR}
+    echo "docker exec awscli aws s3 cp /opt/rsync/builds/genericx86-64_wrlinux_image-glibc-std/genericx86-64_wrlinux_image-glibc-std/bzImage s3://${S3_BUCKET}/${S3_BUCKET_DIR}/"
+    docker exec awscli aws s3 cp /opt/rsync/builds/genericx86-64_wrlinux_image-glibc-std/genericx86-64_wrlinux_image-glibc-std/bzImage s3://${S3_BUCKET}/${S3_BUCKET_DIR}/
+    find "$LOCAL_RSYNC_DIR" \( $search_files \) -exec docker exec awscli aws s3 cp {} s3://${S3_BUCKET}/${S3_BUCKET_DIR}/ \;
+    docker exec awscli aws s3 ls --recursive s3://${S3_BUCKET}/${S3_BUCKET_DIR}
 }
 
 copy2s3 "$@"
